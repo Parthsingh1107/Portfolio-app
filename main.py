c3b05ff5160479c53fd2ea74744c4df01db65b2e
Parth <parthsingh1107@gmail.com>
2023-08-01 12:34:06 -0400

    Day 21: Add description on the new columns

--- main.py
+++ main.py
@@ -5,9 +5,14 @@ st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/parth.png")
+    st.image("images/photo.png")
 
 with col2:
     st.title("Parth Singh")
     content = """Hi, My name is Parth and I'm from India.I live in Canada and work as a software developer"""
     st.info(content)
+
+content2 = """ 
+Below you can find some of the apps I have built in Python. Feel free to contact me!
+"""
+st.write(content2)
