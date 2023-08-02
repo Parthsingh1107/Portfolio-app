d53051b7dfc9a9402388d16f725cf8fa8b99baba
Parth <parthsingh1107@gmail.com>
2023-08-02 15:55:19 -0400

    Day 23: Remove password key

--- send_email.py
+++ send_email.py
@@ -1,19 +1,17 @@
 import smtplib, ssl
+import os
 
-host = "smtp.gmail.com"
-port = 465
 
-username = "parthsingh1107@gmail.com"
-password = "ecfhvtbuneugnpmh"
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
 
-receiver = "parthsingh1107@gmail.com"
-context = ssl.create_default_context()
+    username = "parthsingh1107@gmail.com"
+    password = ""
 
-message = """\
-Subject: Hi!
-How are you?
-Bye
-"""
-with smtplib.SMTP_SSL(host,port, context=context) as server:
-    server.login(username,password)
-    server.sendmail(username,receiver,message)
+    receiver = "parthsingh1107@gmail.com"
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
